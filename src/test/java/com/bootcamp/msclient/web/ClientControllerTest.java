53f79e4d375f1bb9a4c6aa1eae4c359e40f7e35d
llandaur <llandaur@everis.com>
2021-07-06 17:46:50 -0500

    tests created

--- src/test/java/com/bootcamp/msclient/web/ClientControllerTest.java
+++ src/test/java/com/bootcamp/msclient/web/ClientControllerTest.java
@@ -4,6 +4,7 @@ import com.bootcamp.msclient.dto.ClientDTO;
 import com.bootcamp.msclient.repository.ClientRepository;
 import com.bootcamp.msclient.service.ClientService;
 import com.bootcamp.msclient.utils.AppUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -15,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(controllers = ClientController.class)
@@ -35,6 +37,19 @@ class ClientControllerTest {
 
         webTestClient.get()
                 .uri("/clients")
+                .accept(MediaType.APPLICATION_NDJSON)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void testGetClient() {
+        ClientDTO clientDTO = new ClientDTO(1, "name1", 73866786, "pasivo", "su casa", "email@email.com", 97865412);
+        Mockito.when(repository.findById(clientDTO.getId()))
+                .thenReturn(Mono.just(clientDTO).map(AppUtils::dtoToEntity));
+
+        webTestClient.get()
+                .uri("/clients/{id}", clientDTO.getId())
                 .accept(MediaType.APPLICATION_NDJSON)
                 .exchange()
                 .expectStatus().isOk();
